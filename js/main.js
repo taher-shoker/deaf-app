a15a232eb185600187815b4bffee2d4ca858e24e
taher-shoker <taher-elshoker@hotmail.com>
2019-09-16 16:57:38 +0200

    update app

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-/*************************Script for images popup modal ***************************** */
+	/***show sign video for any sign icon in any screen with the tag signvideo_btn ***/
 $(function() {
     $('.popup-img').on('dblclick', function() {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
